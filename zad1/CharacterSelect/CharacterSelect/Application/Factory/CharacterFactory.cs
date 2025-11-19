2db48f71534f0adf8a73713a41fb11754e4b9e9c
unknown <test32905u34272@gmail.com>
2025-11-19 10:28:07 +0100

    sadsadasdasdadasd

--- zad1/CharacterSelect/CharacterSelect/Application/Factory/CharacterFactory.cs
+++ zad1/CharacterSelect/CharacterSelect/Application/Factory/CharacterFactory.cs
@@ -13,6 +13,9 @@ public static class CharacterFactory
             CharacterClass.Warrior => new Warrior(name ?? "Warrior"),
             CharacterClass.Mage    => new Mage(name ?? "Mage"),
             CharacterClass.Rogue   => new Rogue(name ?? "Rogue"),
+            CharacterClass.Lucznik => new Lucznik(name ?? "Lucznik"),
+            CharacterClass.Bard => new Bard(name ?? "Bard"),
+            CharacterClass.Druid => new Druid(name ?? "Druid"),
             _ => throw new ArgumentOutOfRangeException(nameof(cls), "Nieznana klasa postaci.")
         };
     }
--- zad1/CharacterSelect/CharacterSelect/Domain/Enum/CharacterClass.cs
+++ zad1/CharacterSelect/CharacterSelect/Domain/Enum/CharacterClass.cs
@@ -4,5 +4,8 @@ public enum CharacterClass
 {
     Warrior = 1,
     Mage = 2,
-    Rogue = 3
+    Rogue = 3,
+    Lucznik = 4,
+    Bard = 5,
+    Druid = 6
 }
